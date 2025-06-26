6e825aa6492ba0579818f461f008801f8d69c79f
Lengqiang Lin <RK1234559@Gmail.com>
2025-06-25 21:13:30 -0400

    add default location comment Boston

--- app/src/main/java/edu/umb/cs443/MainActivity.kt
+++ app/src/main/java/edu/umb/cs443/MainActivity.kt
@@ -142,7 +142,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             Log.d(DEBUG_TAG, "The query URL is: $query")
             // Log.i("MyTag", query);
             GlobalScope.launch {
-                var jStr = downloadUrl(query)
+                var jStr = downloadUrl(query).replace("[^\\p{Print}]".toRegex(), "")
                 Log.d(DEBUG_TAG, "The response is: $jStr")
                 val ll: LatLng = processJStr(jStr)!!
 
